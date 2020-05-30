91c91650f76b3378d5a010a360f81762fcc809ff
diochagas23 <diogeneschagas20@gmail.com>
2020-05-30 17:20:02 -0300

    Aula de Switch Case

---
+++ teste/src/main/java/br/com/teste/AulaSwitchCase.java
@@ -0,0 +1,36 @@
+package br.com.teste;
+
+import java.util.Scanner;
+
+public class AulaSwitchCase {
+    public static void main(String[] args) {
+        // Alternativas para IDF's e ELSE aninhados
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(">>AULA DE SWITCH CASE<<");
+
+        int diaDaSemana;
+        System.out.println("Informe um dia da semana: (1-7): ");
+        diaDaSemana = scanner.nextInt();
+
+        switch (diaDaSemana) {
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("Dia útil");
+                break;
+            case 1:
+            case 7:
+                System.out.println("Final de semana");
+                break;
+            default:
+                System.out.println("O dia escolhido não é válido.");
+                break;
+        }
+
+        scanner.close();
+
+    }
+}
