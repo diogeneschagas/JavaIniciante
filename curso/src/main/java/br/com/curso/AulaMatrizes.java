de73ebd0489d5f5c3b3eef9b9ea3bbe677f50c64
diochagas23 <diogeneschagas20@gmail.com>
2020-08-22 21:32:40 -0300

    aula de matrizes e exercício

---
+++ curso/src/main/java/br/com/curso/AulaMatrizes.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class AulaMatrizes {
+    public static void main(String[] args) {
+        double [][] notasAlunos = new double[2][4];
+
+        notasAlunos[0][0]= 9.5;
+        notasAlunos[0][1]= 7.5;
+        notasAlunos[0][2]= 9.5;
+        notasAlunos[0][3]= 3.67;
+
+        notasAlunos[1][0]= 2.5;
+        notasAlunos[1][1]= 10;
+        notasAlunos[1][2]= 8.3;
+        notasAlunos[1][3]= 4;
+
+        System.out.println(notasAlunos[0][3]);
+
+    }
+
+}
---
+++ curso/src/main/java/br/com/curso/ExercicioMatrizes.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class ExercicioMatrizes {
+    public static void main(String[] args) {
+
+        String usuario;             // Variável que receberá o nome do 'admin' do sistema
+
+        int alunos;                 // A quantidade de alunos será dada na entrada  'quantAlunos'
+        int quantidadeNotas = 4;    // Esta pode ser considerada uma "regra de negócio" onde nesta escola é obrigatório que o aluno tenha 4 notas
+
+        // Inicialização dos leitores de dados que serão passados na entrada
+        Scanner nomeUsuario = new Scanner(System.in);
+        Scanner quantAlunos = new Scanner(System.in);
+        Scanner notas = new Scanner(System.in);
+
+        System.out.println(">> EXERCÍCIO DE MATRIZES <<");            // Título da aula
+        System.out.println("Como gostaria que te chamássemos? ");
+        usuario = nomeUsuario.next();                            // Recebendo o nome do usuário
+        System.out.println("Boas vindas, " +usuario+"!");       // Mensagem de boas vindas
+
+        System.out.println("Informe a quantidade de alunos desejada: ");    //recebendo a quantidade de alunos
+        alunos = quantAlunos.nextInt();    // Como a variável iniciou com zero, vou incrementar o valor que digitar
+
+        double [][] notasAlunos = new double[alunos][quantidadeNotas];      // Matriz que abriga as notas e quantidade de alunos
+        for(int x = 0; x < alunos; x++){        // Laçço que entra na matriz da qtd. de alunos
+            System.out.println("Informe as notas do Aluno " + (x+1) + ": ");
+            double soma = 0;
+            for(int y = 0; y < quantidadeNotas; y++){       // Laço que entra na matriz da quantidade de notas
+                System.out.println("Nota " + (y+1) + ": ");
+                notasAlunos[x][y] = notas.nextDouble();     // captura das notas
+                soma += notasAlunos[x][y];
+            }
+            double media = soma / quantidadeNotas;      // cálculo da média
+            String resultadoFinal = "";
+            if(media >= 7){             // Verificação se o aluno foi 'aprovado' ou 'reprovado'
+                resultadoFinal = "Certô, miseravi!";
+            }else {
+                resultadoFinal = "não consegue, né?!";
+            }
+            System.out.println("A média do aluno " + (x+1) + " foi " + media + ". Portanto, " + resultadoFinal);       // Resultado impresso
+
+
+        }
+
+
+
+    }
+}
